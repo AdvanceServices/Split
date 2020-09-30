5b6bcac3a3fdf5f29429c11989ffdac8bffbad86
mafragias <mafragias@hotmail.com>
2020-09-30 11:23:36 +0300

    Name Update 30/09/20

--- src/main/java/com/smartupds/rdfsplit/Splitter.java
+++ src/main/java/com/smartupds/rdfsplit/Splitter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.smartupds.rdfsplitter;
+package com.smartupds.rdfsplit;
 
 import java.io.File;
 import java.io.FileInputStream;
